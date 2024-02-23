f933765f52ce099f2ac6862db4ed6276ccd06240
a4Anubhav <anubhavmalik.dx@gmail.com>
2024-02-23 13:14:04 +0530

    Design Tabbing 23-02-2024 V1

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 import "./style.css";
 import FooterNav from "../footer/footerNav";
 import Modal from "react-bootstrap/Modal";
+
 const Dashboard = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  // Tabbing
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
   return (
     <div className="DashboardBg">
       <div className="dd-none dd-block">
@@ -60,31 +68,44 @@ const Dashboard = () => {
                   <p className="WeekDate">18</p>
                 </div>
               </div>
+
               <div className="FourTaskShow">
                 <div className="Pending">
-                  <a className="#">
-                    <div className="YellowBg active">3</div>
+                  <li
+                    onClick={() => handleClick(0)}
+                    className={activeIndex === 0 ? "active" : ""}
+                  >
+                    <div className="YellowBg">3</div>
                     <p>Pending</p>
-                  </a>
+                  </li>
                 </div>
 
                 <div className="Pending">
-                  <a className="#">
+                  <li
+                    onClick={() => handleClick(1)}
+                    className={activeIndex === 1 ? "active" : ""}
+                  >
                     <div className="RedBg">0</div>
                     <p>Cancel</p>
-                  </a>
+                  </li>
                 </div>
                 <div className="Pending">
-                  <a className="#">
+                  <li
+                    onClick={() => handleClick(2)}
+                    className={activeIndex === 2 ? "active" : ""}
+                  >
                     <div className="GreenBg">1</div>
                     <p>Completed</p>
-                  </a>
+                  </li>
                 </div>
                 <div className="Pending">
-                  <a className="#">
+                  <li
+                    onClick={() => handleClick(3)}
+                    className={activeIndex === 3 ? "active" : ""}
+                  >
                     <div className="BlueBg">4</div>
                     <p>Total</p>
-                  </a>
+                  </li>
                 </div>
               </div>
             </div>
@@ -92,61 +113,56 @@ const Dashboard = () => {
           <div className="OrderCreate">
             <a href="/final-job-detail">
               <h2>Celeste Gomez</h2>
-               </a>
-              <div className="OrderDetailsInfo">
-                <div className="InnerInfo">
-                  <img className="img-fluid" src="/assets/call-mess.png" />
-                  <span>9876543210</span>
+            </a>
+            <div className="OrderDetailsInfo">
+              <div className="InnerInfo">
+                <img className="img-fluid" src="/assets/call-mess.png" />
+                <span>9876543210</span>
+              </div>
+              <div className="InnerInfo">
+                <img className="img-fluid" src="/assets/Home_icon.png" />
+                <span>
+                  Quia Ispum Voluptat, Officiis accusantium Veniam sit dolor,
+                  Ispum autem dicta fu,Vanuatu, 69833
+                </span>
+              </div>
+              <div className="InnerInfo">
+                <img className="img-fluid" src="/assets/Read-icon.png" />
+                <span>Debra Melton</span>
+              </div>
+              <div className="ClockOrRead">
+                <div className="InnerInfo w-50">
+                  <img className="img-fluid" src="/assets/Clock.png" />
+                  <span>09:00 am</span>
                 </div>
-                <div className="InnerInfo">
-                  <img className="img-fluid" src="/assets/Home_icon.png" />
-                  <span>
-                    Quia Ispum Voluptat, Officiis accusantium Veniam sit dolor,
-                    Ispum autem dicta fu,Vanuatu, 69833
-                  </span>
+                <div className="InnerInfo w-50">
+                  <img className="img-fluid" src="/assets/Reading.png" />
+                  <span>PRHU20202290027</span>
                 </div>
-                <div className="InnerInfo">
-                  <img className="img-fluid" src="/assets/Read-icon.png" />
-                  <span>Debra Melton</span>
+              </div>
+              <div className="Bottom-button">
+                <div className="w-40">
+                  <button
+                    variant="primary"
+                    onClick={handleShow}
+                    className="PurpulBtnClock btn btn-btn"
+                  >
+                    <img className="img-fluid" src="/assets/Clock-white.png" />
+                    Start
+                  </button>
                 </div>
-                <div className="ClockOrRead">
-                  <div className="InnerInfo w-50">
-                    <img className="img-fluid" src="/assets/Clock.png" />
-                    <span>09:00 am</span>
-                  </div>
-                  <div className="InnerInfo w-50">
-                    <img className="img-fluid" src="/assets/Reading.png" />
-                    <span>PRHU20202290027</span>
-                  </div>
+                <div className="w-30">
+                  <button className="YellowBtn btn btn-btn">
+                    <img className="img-fluid" src="/assets/Clock-Time.png" />
+                  </button>
                 </div>
-                <div className="Bottom-button">
-                  <div className="w-40">
-                    <button variant="primary" onClick={handleShow} className="PurpulBtnClock btn btn-btn">
-                      <img
-                        className="img-fluid"
-                        src="/assets/Clock-white.png"
-                      />
-                      Start
-                    </button>
-                  </div>
-                  <div className="w-30">
-                    <button className="YellowBtn btn btn-btn">
-                      <img className="img-fluid" src="/assets/Clock-Time.png" />
-                    </button>
-                  </div>
-                  <div className="w-30">
-                    
-                      <div className="YellowBtn btn btn-btn">
-                        <img
-                          className="img-fluid"
-                          src="/assets/Anti-clock.png"
-                        />
-                      </div>
-                   
+                <div className="w-30">
+                  <div className="YellowBtn btn btn-btn">
+                    <img className="img-fluid" src="/assets/Anti-clock.png" />
                   </div>
                 </div>
               </div>
-           
+            </div>
           </div>
           <div className="OrderCreate mt-3">
             <a href="/job-details">
@@ -179,35 +195,23 @@ const Dashboard = () => {
                 </div>
                 <div className="Bottom-button">
                   <div className="w-40">
-                    
-                      <button className="PurpulBtnClock  btn btn-btn">
-                        <img
-                          className="img-fluid"
-                          src="/assets/Clock-white.png"
-                        />
-                        Start
-                      </button>
-                    
+                    <button className="PurpulBtnClock  btn btn-btn">
+                      <img
+                        className="img-fluid"
+                        src="/assets/Clock-white.png"
+                      />
+                      Start
+                    </button>
                   </div>
                   <div className="w-30">
-                   
-                      <button className="YellowBtn btn btn-btn">
-                        <img
-                          className="img-fluid"
-                          src="/assets/Clock-Time.png"
-                        />
-                      </button>
-                    
+                    <button className="YellowBtn btn btn-btn">
+                      <img className="img-fluid" src="/assets/Clock-Time.png" />
+                    </button>
                   </div>
                   <div className="w-30">
-                    
-                      <div className="YellowBtn btn btn-btn">
-                        <img
-                          className="img-fluid"
-                          src="/assets/Anti-clock.png"
-                        />
-                      </div>
-                   
+                    <div className="YellowBtn btn btn-btn">
+                      <img className="img-fluid" src="/assets/Anti-clock.png" />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -220,9 +224,7 @@ const Dashboard = () => {
           <Modal.Header closeButton>
             <Modal.Title> Work Order</Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-            Are you want to start work order
-          </Modal.Body>
+          <Modal.Body>Are you want to start work order</Modal.Body>
         </Modal>
       </div>
     </div>
