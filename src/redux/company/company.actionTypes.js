8c7d98d545ffbbe8c3ff9f56d2b1180396fcad21
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-03-26 12:26:29 +0530

    login page company logo fix

--- src/redux/company/company.actionTypes.js
+++ src/redux/company/company.actionTypes.js
@@ -2,3 +2,4 @@ export const getCompanyId="GET_COMPANY_ID";
 export const getTopBarPermission="GET_TOP_BAR_PERMISSION";
 export const getTaxValue="GET_TAX_VALUE";
 export const companyLogout="COMPANY_LOGOUT";
+export const companyLogo= "COMPANY_LOGO";
