8ff58ae3b6bc8a1e529959b89a2d80c794701a68
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-04-01 12:38:44 +0530

    bug of negative adhoc items fixed

--- src/pages/Remarks/Remarks.js
+++ src/pages/Remarks/Remarks.js
@@ -8,6 +8,7 @@ import { formatDateString } from "../../utils/format";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { CommentSchema } from "../../ValidationSchema/Comments";
 import TextError from "../../utils/TextError";
+import CircleLoading from "../../components/CircleLoading";
 
 function Remarks() {
   const navigate = useNavigate();
@@ -15,18 +16,19 @@ function Remarks() {
   const companyGlobalState = useSelector((state) => state.companyModule);
   console.log(userGlobalState, companyGlobalState);
   const [loading, setLoading] = useState(false);
-  const [originalApiCommentDetails, setOriginalApiCommentDetails] = useState(
-    []
-  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [originalApiCommentDetails, setOriginalApiCommentDetails] = useState([]);
   const initialValues = {
     comment: "",
   };
   // API Call for details
   const getCommentListAPICall = async (id, token) => {
     // setLoading(true);
+    setIsLoading(true);
     const result = await getCommentList(id, token);
     console.log("result", result);
     // setLoading(false);
+    setIsLoading(false);
     if (result.error) {
       navigate("/");
     } else {
@@ -35,10 +37,7 @@ function Remarks() {
   };
   useEffect(() => {
     if (userGlobalState.details.token) {
-      getCommentListAPICall(
-        userGlobalState.workerOrderId,
-        userGlobalState.details.token
-      );
+      getCommentListAPICall(userGlobalState.workerOrderId, userGlobalState.details.token);
     } else {
       navigate("/");
     }
@@ -46,17 +45,12 @@ function Remarks() {
   console.log(originalApiCommentDetails);
   const handleAddComment = async (values, actions) => {
     // setLoading(true);
-    const result = await addComment(
-      userGlobalState.workerOrderId,
-      values.comment,
-      userGlobalState.details.token
-    );
+    setIsLoading(true);
+    const result = await addComment(userGlobalState.workerOrderId, values.comment, userGlobalState.details.token);
+    setIsLoading(false);
     actions.resetForm();
     if (!result.error) {
-      getCommentListAPICall(
-        userGlobalState.workerOrderId,
-        userGlobalState.details.token
-      );
+      getCommentListAPICall(userGlobalState.workerOrderId, userGlobalState.details.token);
     }
   };
   const endRef = useRef(null);
@@ -73,101 +67,83 @@ function Remarks() {
   };
   return (
     <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className={Styles.JobDetalTop}>
-          <div className={Styles.TopSection}>
-            <div className={Styles.backArrow}>
-              <a href="/job-details">
-                <svg
-                  height="20"
-                  width="20"
-                  viewBox="0 0 20 20"
-                  aria-hidden="true"
-                  focusable="false"
-                  className="css-8mmkcg"
-                >
-                  <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
-                </svg>
-              </a>
-            </div>
-            <div className={Styles.Greetings}>
-              <h5>Remarks for Works Order at </h5>
-            </div>
+      {loading ? <Loading /> : null}
+      <div className={Styles.JobDetalTop}>
+        <div className={Styles.TopSection}>
+          <div className={Styles.backArrow}>
+            <a href="/job-details">
+              <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-8mmkcg">
+                <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+              </svg>
+            </a>
           </div>
-
-          <div>
-            <div className={Styles.InnerInfo}>
-              <div className={Styles.iconControl}>
-                <img className="" alt="img" src="/assets/Home_icon.png" />
-              </div>
-              <h3>
-                {userGlobalState.workerOrderId}
-                <span>: {userGlobalState.address}</span>
-              </h3>
-            </div>
-          </div>
-
-          <div className={Styles.ControlMain}>
-            {originalApiCommentDetails.length ? (
-              originalApiCommentDetails.map((ele) => {
-                return (
-                  <>
-                    {ele?.commenter_type === "Admin" ? (
-                      <div
-                        className={`${Styles.RemarksBoxPink} ${Styles.RemarksBoxGray}`}
-                      >
-                        <h2>
-                          {ele?.commenter}:{" "}
-                          <span>{formatDateString(ele?.created)}</span>
-                        </h2>
-                        <p style={{wordBreak:"break-all"}}>{ele?.description}</p>
-                      </div>
-                    ) : (
-                      <div className={Styles.RemarksBoxPink}>
-                        <h2>
-                          {ele?.commenter}:{" "}
-                          <span>{formatDateString(ele?.created)}</span>
-                        </h2>
-                        <p style={{wordBreak:"break-all"}}>{ele?.description}</p>
-                      </div>
-                    )}
-                  </>
-                );
-              })
-            ) : (
-              <div className={Styles.NoData}>No Comments.</div>
-            )}
-            {/* add comment section */}
-            <div style={{ marginTop: "100px" }} ref={endRef}></div>
-
-            <Formik
-              initialValues={initialValues}
-              onSubmit={handleAddComment}
-              validationSchema={CommentSchema}
-            >
-              <Form>
-                <div className={Styles.BottomChatFixed}>
-                  <div className={Styles.bgWhite}>
-                    <ErrorMessage component={TextError} name="comment" />
-                  </div>
-                  <div className={Styles.Chatsend}>
-                    <div className={Styles.chatInput}>
-                      <Field type="text" placeholder="Message" name="comment" />
-                    </div>
-
-                    <button type="submit">
-                      <img src="/assets/SendIcon.png" alt="img" />
-                    </button>
-                    {/* <input type="text" placeholder="Message" /> */}
-                  </div>
-                </div>
-              </Form>
-            </Formik>
+          <div className={Styles.Greetings}>
+            <h5>Remarks for Works Order at </h5>
           </div>
         </div>
-      )}
+
+        <div>
+          <div className={Styles.InnerInfo}>
+            <div className={Styles.iconControl}>
+              <img className="" alt="img" src="/assets/Home_icon.png" />
+            </div>
+            <h3>
+              {userGlobalState.workerOrderId}
+              <span>: {userGlobalState.address}</span>
+            </h3>
+          </div>
+        </div>
+
+        <div className={Styles.ControlMain}>
+          {originalApiCommentDetails.length ? (
+            originalApiCommentDetails.map((ele) => {
+              return (
+                <>
+                  {ele?.commenter_type === "Admin" ? (
+                    <div className={`${Styles.RemarksBoxPink} ${Styles.RemarksBoxGray}`}>
+                      <h2>
+                        {ele?.commenter}: <span>{formatDateString(ele?.created)}</span>
+                      </h2>
+                      <p style={{ wordBreak: "break-all" }}>{ele?.description}</p>
+                    </div>
+                  ) : (
+                    <div className={Styles.RemarksBoxPink}>
+                      <h2>
+                        {ele?.commenter}: <span>{formatDateString(ele?.created)}</span>
+                      </h2>
+                      <p style={{ wordBreak: "break-all" }}>{ele?.description}</p>
+                    </div>
+                  )}
+                </>
+              );
+            })
+          ) : (
+            <>{isLoading ? <CircleLoading /> : <div className={Styles.NoData}>No Comments.</div>}</>
+          )}
+          {/* add comment section */}
+          <div style={{ marginTop: "100px" }} ref={endRef}></div>
+
+          <Formik initialValues={initialValues} onSubmit={handleAddComment} validationSchema={CommentSchema}>
+            <Form>
+              <div className={Styles.BottomChatFixed}>
+                <div className={Styles.bgWhite}>
+                  <ErrorMessage component={TextError} name="comment" />
+                </div>
+                <div className={Styles.Chatsend}>
+                  <div className={Styles.chatInput}>
+                    <Field type="text" placeholder="Message" name="comment" />
+                  </div>
+
+                  <button type="submit" disabled={isLoading} className={isLoading ? `${Styles.my_button_loading}` : null}>
+                    <img src="/assets/SendIcon.png" alt="img" />
+                  </button>
+                  {/* <input type="text" placeholder="Message" /> */}
+                </div>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </div>
     </>
   );
 }
