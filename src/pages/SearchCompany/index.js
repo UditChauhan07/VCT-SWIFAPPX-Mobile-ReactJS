a73fe5bbc0423a7e15c5521ddf5f9a1cbf94e27b
“Ripty-Dx” <riptyfullstack.dx@gmail.com>
2024-04-15 09:08:15 +0530

    Search company fix

--- src/pages/SearchCompany/index.js
+++ src/pages/SearchCompany/index.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
+import Modal from "react-bootstrap/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
 import { getCompaniesList } from "../../api/company";
-import { getCompanyDetails, getCompanyId, getCompanyName, getLogoCompany, taxValueOfCompany, topBarPermission } from "../../redux/company/company.actions";
+import {
+  getCompanyDetails,
+  getCompanyId,
+  getCompanyName,
+  getLogoCompany,
+  taxValueOfCompany,
+  topBarPermission,
+} from "../../redux/company/company.actions";
 import { useNavigate } from "react-router-dom";
 import { useInternetStatusCheck } from "../../utils/updation";
+import { SearchIcon } from "../../utils/svg";
 const Index = () => {
   const online = useInternetStatusCheck();
   const globalState = useSelector((state) => state.companyModule);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [companiesList, setCompaniesList] = useState([]);
-
+  const [selectedCompany, setSelectedCompany] = useState();
+  const [noCompanyModalShow, setNoCompanyModalShow] = useState(false);
   const companiesListApiCall = async () => {
     const result = await getCompaniesList();
     if (result.error) alert("You are offline. Reconnecting...");
@@ -28,18 +38,48 @@ const Index = () => {
 
   console.log("globalState", globalState);
   const handleCompanySubmit = () => {
-    if (globalState.company_id) {
+    // if (globalState.company_id) {
+    if (selectedCompany) {
+      dispatch(getCompanyId(selectedCompany.value));
+      dispatch(getCompanyName(selectedCompany.label));
+      dispatch(
+        topBarPermission(
+          companiesList.filter((ele) => ele.id === selectedCompany.value)[0]
+            .topBarPermission
+        )
+      );
+      dispatch(
+        taxValueOfCompany(
+          companiesList.filter((ele) => ele.id === selectedCompany.value)[0].tax
+        )
+      );
+      dispatch(
+        getLogoCompany(
+          companiesList.filter((ele) => ele.id === selectedCompany.value)[0]
+            .company_logo
+        )
+      );
+      dispatch(
+        getCompanyDetails(
+          companiesList.filter((ele) => ele.id === selectedCompany.value)[0]
+        )
+      );
       navigate("/BusinessDetail");
-    } else alert("Pick one company to proceed.");
+    } else setNoCompanyModalShow(true);
   };
 
+  const handleNoCompanySelected = () => setNoCompanyModalShow(false);
   return (
     <>
       <div className="dd-none dd-block p20">
         <div className="vCenter">
           <div className="w-100">
             <div className="logo ">
-              <img className="img-fluid" src="/assets/Swif-logo.png" alt="img" />
+              <img
+                className="img-fluid"
+                src="/assets/Swif-logo.png"
+                alt="img"
+              />
             </div>
             <p className="SearchCompanyText">Search Company Name</p>
             <div className="input-group rounded">
@@ -50,35 +90,67 @@ const Index = () => {
                   value: ele.id,
                   label: ele.name,
                 }))}
-                defaultValue={online ? null : { value: globalState.company_id, label: globalState.company_name }}
+                // defaultValue={
+                //   online
+                //     ? null
+                //     : {
+                //         value: globalState.company_id,
+                //         label: globalState.company_name,
+                //       }
+                // }
                 onChange={(option) => {
-                  dispatch(getCompanyId(option.value));
-                  dispatch(getCompanyName(option.label));
-                  dispatch(topBarPermission(companiesList.filter((ele) => ele.id === option.value)[0].topBarPermission));
-                  dispatch(taxValueOfCompany(companiesList.filter((ele) => ele.id === option.value)[0].tax));
-                  dispatch(getLogoCompany(companiesList.filter((ele) => ele.id === option.value)[0].company_logo));
-                  dispatch(getCompanyDetails(companiesList.filter((ele) => ele.id === option.value)[0]));
+                  setSelectedCompany(option);
+                  console.log(option);
+                  // dispatch(getCompanyId(option.value));
+                  // dispatch(getCompanyName(option.label));
+                  // dispatch(topBarPermission(companiesList.filter((ele) => ele.id === option.value)[0].topBarPermission));
+                  // dispatch(taxValueOfCompany(companiesList.filter((ele) => ele.id === option.value)[0].tax));
+                  // dispatch(getLogoCompany(companiesList.filter((ele) => ele.id === option.value)[0].company_logo));
+                  // dispatch(getCompanyDetails(companiesList.filter((ele) => ele.id === option.value)[0]));
                 }}
               />
 
               <span className="input-group-text border-0" id="search-addon">
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
-                  <path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path>
-                </svg>
+                <SearchIcon />
               </span>
             </div>
-            {globalState.company_id ? (
-              <div className="SubmitButton mt-20">
-                <button className="btn btn-btn SubmitBtnStyle" onClick={handleCompanySubmit}>
-                  Submit
-                </button>
-              </div>
-            ) : null}
+            {/* {globalState.company_id ? ( */}
+            {/* {selectedCompany ? ( */}
+            <div className="SubmitButton mt-20">
+              <button
+                className="btn btn-btn SubmitBtnStyle"
+                onClick={handleCompanySubmit}
+              >
+                Submit
+              </button>
+            </div>
+            {/* ) : null} */}
           </div>
         </div>
       </div>
-      {!online ? <div className="bg-danger fixed-bottom d-flex justify-content-center mt-3">You are offline. Check your internet connection!</div> : null}
+      {!online ? (
+        <div className="bg-danger fixed-bottom d-flex justify-content-center mt-3">
+          You are offline. Check your internet connection!
+        </div>
+      ) : null}
+      {/* Modal for no company selected */}
+      <Modal show={noCompanyModalShow} onHide={handleNoCompanySelected}>
+        <Modal.Header closeButton>
+          <Modal.Title> Alert</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          Please pick one company from list to proceed
+          <div className="d-flex gap-5 mt-3">
+            <button
+              variant="primary"
+              onClick={handleNoCompanySelected}
+              className="PurpulBtnClock w-30 btn btn-btn"
+            >
+              OK
+            </button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
